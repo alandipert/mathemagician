ed4fc5604dbb87d49e4e38536f2a86b0e681bdb2
Gabriel Horner <gabriel.horner@gmail.com>
2012-07-01 12:56:44 -0400

    knock out some tests for #2 :punch:

--- test/mathemagician_test.clj
+++ test/mathemagician_test.clj
@@ -3,11 +3,24 @@
         [mathemagician]))
 
 (deftest testing-big-picture
-  (testing "at a high level that this library does something useful."
-    (is (= 3 (abs -3)))))
+  (testing "at a high level that installed methods of different arities work correctly"
+    (is (= java.lang.Double (class (random))))
+    (is (= 3 (abs -3)))
+    (is (= 9.0 (pow 3 2)))))
 
 (deftest testing-clojurize
   (testing "that the way we munge camelcase is correct"
     (is (= "next-thing-blah" (@#'mathemagician/clojurize "nextThingBlah")))
     (is (= "ieee-remainder" (@#'mathemagician/clojurize "IEEEremainder")))
     (is (= "another-ieee-remainder" (@#'mathemagician/clojurize "anotherIEEEremainder")))))
+
+(deftest testing-fn-impls
+  (testing "a function implementation is included"
+    (is (some
+          #{'cos}
+          (->> (@#'mathemagician/fn-impls #{"random"} Math) (map second)))))
+  (testing "a function implementation is excluded"
+    (is (nil?
+          (some
+            #{'random}
+            (->> (@#'mathemagician/fn-impls #{"random"} Math) (map second)))))))
